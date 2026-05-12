c926e1f9f02b9acbb1d609254b5cc75c79f6765c
ThuaDang <zeeetoy@gmail.com>
2026-05-12 09:48:04 +0200

    Fin de la table
    table finie

--- Partie 1 - Traduction et implantation dun schéma relationnel-20260511/parcoursup_db_create_d14.sql
+++ Partie 1 - Traduction et implantation dun schéma relationnel-20260511/parcoursup_db_create_d14.sql
@@ -1,6 +1,23 @@
+
 drop schema if exists parcoursup cascade;
 create schema parcoursup;
 set schema 'parcoursup';
+
+create type type_bac as enum (
+	'Bac général',
+	'Bac technologique',
+	'Bac professionnel',
+	'Autres'
+);
+
+create type libelle_mention as enum(
+	'Sans information',
+	'Sans mention',
+	'Assez bien',
+	'Bien',
+	'Très bien',
+	'Très bien avec félicitations du jury'
+);
 
 drop table if exists _academie;
 create table _academie(
@@ -40,12 +57,12 @@ create table _regroupement(
 
 drop table if exists _type_bac;
 create table _type_bac(
-	type_bac varchar(30) primary key
+	type_bac type_bac primary key
 );
 
 drop table if exists _mention_bac;
 create table _mention_bac(
-	libelle_mention varchar(30)
+	libelle_mention libelle_mention primary key
 );
 
 drop table if exists _departement;
@@ -58,8 +75,8 @@ create table _departement(
 drop table if exists _commune;
 create table _commune(
 	commune_nom varchar(30) primary key,
-	departement_code varchar(30) references _departement(departement_code)
-	);
+	departement_code varchar(30) references _departement(departement_code) 	
+);
 
 drop table if exists _formation;
 create table _formation(
@@ -76,3 +93,40 @@ create table _formation(
 	commune_nom varchar(30) references _commune(commune_nom)
 );
 
+drop table if exists _admissions_selon_type_neo_bac;
+create table _admissions_selon_type_neo_bac(
+	effectif_candidat_neo_bac_classes int,
+	cod_aff_form varchar(30) references _formation(cod_aff_form),
+	type_bac type_bac references _type_bac(type_bac),
+	session_annee int references _session(session_annee),
+	constraint admissions_selon_type_neo_bac_PK primary key (cod_aff_form, type_bac, session_annee)
+);
+
+drop table if exists _effectif_selon_mention;
+create table _effectif_selon_mention(
+	effectif_admis_neo_bac_selon_mention int,
+	libelle_mention libelle_mention references _mention_bac(libelle_mention),
+	cod_aff_form varchar(30) references _formation(cod_aff_form),
+	session_annee int references _session(session_annee),
+	constraint effectif_selon_mention_PK primary key (libelle_mention, cod_aff_form, session_annee)
+);
+
+drop table if exists _admissions_generalites;
+create table _admissions_generalites(
+	selectivite varchar(30),
+	capacite int,
+	effectif_total_candidats int,
+	effectif_total_candidates int,
+	session_annee int references _session(session_annee),
+	cod_aff_form varchar(30) references _formation(cod_aff_form),
+	constraint admissions_generalites_PK primary key (session_annee, cod_aff_form)
+);
+
+drop table if exists _rang_dernier_appele_selon_regroupement;
+create table _rang_dernier_appele_selon_regroupement(
+	rang_dernier_appele int,
+	libelle_regroupement varchar(30) references _regroupement(libelle_regroupement),
+	session_annee int references _session(session_annee),
+	cod_aff_form varchar(30) references _formation(cod_aff_form),
+	constraint rang_dernier_appele_selon_regroupement_PK primary key (libelle_regroupement, session_annee, cod_aff_form)
+)
